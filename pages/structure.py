cf05cd415c27d07b2b01b2778ec5914bd43ace46
Andrestart <andres.perez.tercero@gmail.com>
2021-12-16 14:15:32 +0100

    up and running

--- pages/structure.py
+++ pages/structure.py
@@ -6,7 +6,6 @@ def app():
     st.sidebar.image("images/tools.png", use_column_width=True)
 
     #IMGS
-    dataimg = Image.open("images/data.PNG")
     sql = Image.open("images/sqlschema.PNG")
 
     #WHY
@@ -26,11 +25,10 @@ def app():
     st.write("""I followed the following structure: \n""")
     st.write("""- Getting historical price data making a request to an [API](https://en.wikipedia.org/wiki/API) called [Agrifood](https://agridata.ec.europa.eu/extensions/DataPortal/API_Documentation.html).\n""")
     st.write("""- Saving it into different food categories to simplify it. After cleaning my dataframe, it looked like this:\n""")
-    st.image(dataimg, use_column_width=False)
     st.write("""- Separating the dataframe per product and preparing it for predictions.\n""")
     st.write("""- Predicting the food prices per product.\n""")
+    st.write("""- Creating a database to store my data.\n""")
     st.write("""- Getting today's food prices from different supermarkets (conditional scrapping).\n""")
-    st.write("""- Creating a database to store the historical and today's data and the graphs generated.\n""")
     st.image(sql, use_column_width=True)
     st.write("""- Making a user-friendly web to show the graphs and conclusions.\n""")
     st.write("""- Making the whole process executable so the end user can just run the program and get all updated information everyday without knowing how the code looks like.""")
